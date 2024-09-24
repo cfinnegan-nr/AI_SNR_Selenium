3fae439d804b0929ede9c048e8f95afbeeb9aa10
Ciaran Finnegan <ciaran@feefinnegan.com>
2024-09-24 15:05:42 +0100

    First code elements...

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+# Import OpenAI Environment Variables
+from openaienvvars import OPENAI_API_KEY
 
+# Import the necessary libraries
 import openai
 from decouple import config
 import openpyxl
+
+# Set up OpenAI API credentials
+openai.api_key = config('OPENAI_API_KEY')
+
